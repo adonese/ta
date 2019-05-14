65619987e58041852300875e8c006042f2b8b1fa
Mohamed Yousif <mmbusif@gmail.com>
2019-05-15 01:44:11 +0200

    adding html to TA service

--- ta.py
+++ ta.py
@@ -1,24 +1,59 @@
 from starlette.applications import Starlette
-from starlette.responses import JSONResponse
+from starlette.responses import JSONResponse, RedirectResponse
 import uvicorn
 from pin import PinBlock
 from utils import http_errors_or_ok
 from utils import RequestFields
+from starlette.templating import Jinja2Templates
+import typesystem
 
+# templates dir
+forms = typesystem.Jinja2Forms(package="bootstrap4")
+templates = Jinja2Templates(directory="templates")
 
 app = Starlette(debug=True)
 
 @app.route("/pin", methods=["POST"])
-@app.route("/", methods=["POST"])
+@app.route("/calculate_pin", methods=["POST"])
 async def pin_service(request):
-    b = await request.json()
-    p, errors = RequestFields.validate_or_error(b)
+    try:
+        b = await request.json()
+    except Exception as e:
+        return JSONResponse({"message": "Empty or malformed Json"}, 400, media_type="application/json")
+    data, errors = RequestFields.validate_or_error(b)
  
     if errors:
         return JSONResponse(dict(errors), 400, media_type="application/json")
     pin_calculation = PinBlock(b.get("pin"), b.get("pan"), b.get("twk"), b.get("tmk"))
     pin = pin_calculation.encrypted_pin_block()
     return JSONResponse({"pin_block": pin}, 200, media_type="application/json")
+
+@app.route("/", methods=["GET"])
+async def homepage(request):
+    form = forms.Form(RequestFields)
+    context = {"request": request, "form": form}
+    return templates.TemplateResponse("index.html", context)
+
+@app.route("/", methods=["POST"])
+async def submit(request):
+    form = await request.form()
+    data, errors = RequestFields.validate_or_error(form)
+    print(errors)
+    if errors:
+        form = forms.Form(RequestFields, values=data, errors=errors)
+        context = {"request": request, "form": form}
+        return templates.TemplateResponse("index.html", context)
+
+    pin_calculation = PinBlock(form.get("pin"), form.get("pan"), form.get("twk"), form.get("tmk"))
+    pin = pin_calculation.encrypted_pin_block()       
+    context = {"request": request, "form": form, "pin": pin}
+    return templates.TemplateResponse("success.html", context)
+
+@app.route("/success", methods=["GET"])
+async def success(request):
+    context = {"request": request}
+    return templates.TemplateResponse("success.html", context)
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8080)
