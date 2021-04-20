629e8a3b1524e998d6ab5dedcf845990e5afeff1
Omar M. Osman <omarosmandev@gmail.com>
2021-04-20 06:36:20 +0200

    add a filed class HexNumber

--- utils.py
+++ utils.py
@@ -4,11 +4,28 @@ return corresponding http errors
 import typesystem
 import uuid 
 
+# custom schema field
+class HexNumber(typesystem.String):
+    # add new error-code and error message 
+    typesystem.String.errors['hex_number'] = 'Must be a hexadicimal number'
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+    
+    def validate(self, value, **kwargs):
+        try:
+            bytes.fromhex(value)
+        except Exception as e:
+            raise self.validation_error("hex_number")
+        return super().validate(value, **kwargs)
+
+
 class RequestFields(typesystem.Schema):
     pin = typesystem.String(title="PIN", max_length=4, min_length=4)
     pan = typesystem.String(title="PAN", max_length=19, min_length=16)
-    twk = typesystem.String(title="Working Key", max_length=16, min_length=16)
-    tmk = typesystem.String(title="Master Key", max_length=16, min_length=16)
+    twk = HexNumber(title="Working Key", max_length=16, min_length=16)
+    tmk = HexNumber(title="Master Key", max_length=16, min_length=16)
+
 
 class http_errors_or_ok:
     """
